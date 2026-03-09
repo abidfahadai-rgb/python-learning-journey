0faddf493043c68b788eea007e4510f5c2799829
FahadCodes <abidfahad.ai@gmail.com>
2026-03-09 21:38:20 +0600

    Create even_checker.py

---
+++ Phase 1/even_checker.py
@@ -0,0 +1,17 @@
+#
+# File: practical2ex10.py
+# Author: Abid Fahad Khan
+# Email Id: your_email@example.com
+# Description: Practical 2, exercise 10. Checks if a number is even.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+# Ask the user to enter a number
+number = int(input("Enter an integer: "))
+
+# Check if the number is even
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is not even")
