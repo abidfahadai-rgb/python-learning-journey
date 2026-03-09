f9904406076047ecdbcaa1acbdc60b1da1e3b162
FahadCodes <abidfahad.ai@gmail.com>
2026-03-09 21:35:48 +0600

    Create square_calculator.py

---
+++ Phase 1/square_calculator.py
@@ -0,0 +1,21 @@
+#
+# File: practical2ex5.py
+# Author: Fahad
+# Email Id: your_email@example.com
+# Description: Calculates the square of a number.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+# Ask the user to enter a number
+number = int(input("Enter an integer: "))
+
+# Calculate the square
+square = number ** 2
+
+# Display the result
+print("The square of the number is:", square)
+
+#this can be used as well 
+#import math
+#math.sqrt(number)
