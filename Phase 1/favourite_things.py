b629a89f32cacc4f154e3500fcef7f73bc5356ec
FahadCodes <abidfahad.ai@gmail.com>
2026-03-09 21:32:06 +0600

    Update favourite_things.py

--- Phase 1/favourite_things.py
+++ Phase 1/favourite_things.py
@@ -1,1 +1,11 @@
+# Favourite colour
+colour = input("Enter your favourite colour: ")
+print("That's a lovely colour!")
 
+# Favourite football team
+team = input("Enter your favourite football team: ")
+print("That's my favourite team too!")
+
+# Favourite drink
+drink = input("Enter your favourite drink: ")
+print("Better not have too many!")
