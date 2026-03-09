3bcfc23ae5d13b63c0795147b1237cbe63984d48
FahadCodes <abidfahad.ai@gmail.com>
2026-03-09 21:34:12 +0600

    Create three_number_sum.py

---
+++ Phase 1/three_number_sum.py
@@ -0,0 +1,19 @@
+#
+# File: practical2ex3.py
+# Author: Fahad
+# Email Id: your_email@example.com
+# Description: Adds three numbers entered by the user.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+# Ask the user to enter three numbers
+num1 = int(input("Please enter number 1: "))
+num2 = int(input("Please enter number 2: "))
+num3 = int(input("Please enter number 3: "))
+
+# Add the numbers
+total = num1 + num2 + num3
+
+# Display the result
+print("The sum is:", total)
