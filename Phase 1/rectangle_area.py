9b26b7f88418da7d653419adf8b0410fab52a419
FahadCodes <abidfahad.ai@gmail.com>
2026-03-09 21:33:09 +0600

    Create rectangle_area.py

---
+++ Phase 1/rectangle_area.py
@@ -0,0 +1,20 @@
+#
+# File: practical2ex2.py
+# Author: Fahad
+# Email Id: your_email@example.com
+# Description: Calculates the area of a rectangle.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+# Ask the user to enter the width
+width = int(input("Enter the width: "))
+
+# Ask the user to enter the height
+height = int(input("Enter the height: "))
+
+# Calculate the area
+area = width * height
+
+# Display the result
+print("The area is:", area)
