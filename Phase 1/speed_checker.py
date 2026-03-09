4a196f21d11d3ccc9c4dd950c733cb172bb8097f
FahadCodes <abidfahad.ai@gmail.com>
2026-03-09 21:37:23 +0600

    Create speed_checker.py

---
+++ Phase 1/speed_checker.py
@@ -0,0 +1,17 @@
+#
+# File: practical2ex9.py
+# Author: Fahad
+# Email Id: your_email@example.com
+# Description: Practical 2, exercise 9. Checks if a car is speeding.
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
+# Ask the user to enter the speed
+speed = int(input("Enter the speed of the car: "))
+
+# Check the speed
+if speed >= 65:
+    print("Too fast - Fine!")
+else:
+    print("No fine")
