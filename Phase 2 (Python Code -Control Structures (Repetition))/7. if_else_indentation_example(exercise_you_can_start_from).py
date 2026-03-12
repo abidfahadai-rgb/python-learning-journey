deeeddc3e810fdd6be85f1858d847664b9ff0491
FahadCodes <abidfahad.ai@gmail.com>
2026-03-12 13:56:37 +0600

    Updated

---
+++ Phase 2 (Python Code -Control Structures (Repetition))/7. if_else_indentation_example(exercise_you_can_start_from).py
@@ -0,0 +1,17 @@
+#
+# File: if_else_indentation_example.py
+# Author: Fahad
+# Description: Program demonstrating proper indentation using an if-else statement.
+#
+# from here we moved on to exercises on this phase's topic after breifly touching the topic
+
+a = 1
+b = 1
+c = 1
+
+if a == b:
+    print("In if statement.")
+    print("Equal.")
+else:
+    print("In else statement.")
+    print("Not equal.")
